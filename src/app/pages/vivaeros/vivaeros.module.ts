f7defec1bbeb41ea928fd19a19c61d45e855cf37
Venkatesh Penchala <venkatesh.penchala@evosoft.com>
2023-08-17 10:08:45 +0200

    adding map

--- src/app/pages/vivaeros/vivaeros.module.ts
+++ src/app/pages/vivaeros/vivaeros.module.ts
@@ -7,12 +7,22 @@ import { NbCardModule } from "@nebular/theme";
 import { VivaerosPieComponent } from "./echarts/piechart.component";
 import { VivaerosDashboardComponent } from "./vivaeros.component";
 import { VivaerosBarComponent } from "./echarts/barchart.component";
+import { VivaerosCountryOrdersComponent } from "./country-orders/country-orders.component";
+import { VivaerosProgressSectionComponent } from "./progress-section/progress-section.component";
+import { VivaerosCountryOrdersChartComponent } from "./country-orders/chart/country-orders-chart.component";
+import { VivaerosCountryOrdersMapComponent } from "./country-orders/map/country-orders-map.component";
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { VivaerosCountryOrdersMapService } from "./country-orders/map/country-orders-map.service";
 
 
 const components = [
   VivaerosPieComponent,
   VivaerosDashboardComponent,
-  VivaerosBarComponent
+  VivaerosBarComponent,
+  VivaerosCountryOrdersComponent,
+  VivaerosProgressSectionComponent,
+  VivaerosCountryOrdersChartComponent,
+  VivaerosCountryOrdersMapComponent
 ];
 
 @NgModule({
@@ -22,7 +32,11 @@ const components = [
     NgxChartsModule,
     ChartModule,
     NbCardModule,
+    LeafletModule,
   ],
   declarations: [ ...components],
+  providers: [
+    VivaerosCountryOrdersMapService,
+  ]
 })
 export class VivaerosModule {}
